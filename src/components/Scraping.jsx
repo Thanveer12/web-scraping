aa702607b27143bdd4ce7ea7a2e478cb6ea55b10
Thanveer Ahamed A <thanveerahamed1225@gmail.com>
2023-08-31 16:55:24 +0530

    filter all the datas shown in frontend

--- src/components/Scraping.jsx
+++ src/components/Scraping.jsx
@@ -15,23 +15,103 @@ function Scraping() {
                 const sanitizedContent = data.replace(/&(?!#?\w+;)/g, '&amp;');
                 const dom = new DOMParser().parseFromString(sanitizedContent, "text/html");
                 console.log(dom)
-                // const bodyElement = document.getElementsByTagName('body')[0];
-    
-                // const headElement = document.getElementsByTagName('head')[0];
-    
-                // const head = dom.querySelectorAll('head')[0].children;
-                // for (let i = 0; i< head.length ; i ++) {
-                //   headElement.append(head[i]);
-                // }
-                // const body = dom.querySelectorAll('body')[0].children;
-                // for (let i = 0; i< body.length ; i ++) {
-                //   bodyElement.append(body[i]);
-                // }
-               
-               
-                // console.log(dom.all[0])
-                // // document.append(dom.all[0])
-                // console.log(data)
+
+                const button = dom.getElementsByTagName('button')
+                const anchor = dom.getElementsByTagName('a');
+                const script = dom.getElementsByTagName('script')
+                const meta = dom.getElementsByTagName('meta');
+                const link = dom.getElementsByTagName('link');
+                const input = dom.getElementsByTagName('input');
+
+                const scraperContainer = document.getElementById('scraper-container')
+                const linkDiv = document.createElement('div')
+                const linkHeading = document.createElement('h1');
+                linkHeading.innerHTML = 'Link';
+                linkDiv.append(linkHeading);
+                const linkUl = document.createElement('ul');
+                for (let i = 0; i < link.length; i++){
+                    // scraperContainer.append(button[i]);
+                    const linkLi = document.createElement('li');
+                    linkLi.innerHTML = '<b>href-</b>' + link[i].href + '  <b>id-</b>' + link[i].id;
+                    linkUl.append(linkLi);
+                }
+                linkDiv.append(linkUl);
+                scraperContainer.append(linkDiv);
+
+                const metaDiv = document.createElement('div')
+                const metaHeading = document.createElement('h1');
+                metaHeading.innerHTML = 'Meta';
+                metaDiv.append(metaHeading);
+                const metaUl = document.createElement('ul');
+                for (let i = 0; i < meta.length; i++){
+                    // scraperContainer.append(button[i]);
+                    const metaLi = document.createElement('li');
+                    metaLi.innerHTML = '<b>content-</b>' + meta[i].content + '  <b>property-</b>' + meta[i].property + ' <b>name-</b>' + meta[i].name;
+                    metaUl.append(metaLi);
+                }
+                metaDiv.append(metaUl);
+                scraperContainer.append(metaDiv);
+
+
+                const buttonDiv = document.createElement('div')
+                const buttonHeading = document.createElement('h1');
+                buttonHeading.innerHTML = 'Link';
+                buttonDiv.append(buttonHeading);
+                const buttonUl = document.createElement('ul');
+                for (let i = 0; i < button.length; i++){
+                    const buttonLi = document.createElement('li');
+                    buttonLi.innerHTML = button[i].innerText;
+                    // scraperContainer.append(button[i]);
+                    buttonUl.append(buttonLi)
+                }
+                buttonDiv.append(buttonUl)
+                scraperContainer.append(buttonDiv);
+
+                const anchorDiv = document.createElement('div')
+                const anchorHeading = document.createElement('h1');
+                anchorHeading.innerHTML = 'Anchor';
+                anchorDiv.append(anchorHeading);
+                const anchorUl = document.createElement('ul');
+                for (let i = 0; i < anchor.length; i++){
+                    // scraperContainer.append(anchor[i]);
+                    const anchorLi = document.createElement('li');
+                    anchorLi.innerHTML = '<b>href-</b>' + anchor[i].href + ' <b>content-</b>'+ anchor[i].innerText;
+                    anchorUl.append(anchorLi);
+                }
+                anchorDiv.append(anchorUl);
+                scraperContainer.append(anchorDiv);
+
+
+
+                const scriptDiv = document.createElement('div')
+                const scriptHeading = document.createElement('h1');
+                scriptHeading.innerHTML = 'Script';
+                scriptDiv.append(scriptHeading);
+                const scriptUl = document.createElement('ul');
+                for (let i = 0; i < script.length; i++){
+                    // scraperContainer.append(script[i]);
+                    const li = document.createElement('li');
+                    li.innerHTML = '<b>src-</b>' + script[i].src + ' <b>id-</b>' + script[i].id ;
+                    // scriptDiv.innerHTML += '<br>'+script[i].id + '---' + script[i].src;
+                    scriptUl.append(li);
+                }
+                scriptDiv.append(scriptUl);
+                scraperContainer.append(scriptDiv)
+
+
+                const inputDiv = document.createElement('div')
+                const inputHeading = document.createElement('h1');
+                inputHeading.innerHTML = 'Input';
+                inputDiv.append(inputHeading);
+                const inputUl = document.createElement('ul');
+                for (let i = 0; i < input.length; i++){
+                    const inputLi = document.createElement('li');
+                    inputLi.innerHTML = '<b>name-</b>' + input[i].name + ' <b>placeHolder-</b>' + input[i].placeholder;
+                    inputUl.append(inputLi);
+                }
+                inputDiv.append(inputUl);
+                scraperContainer.append(inputDiv);
+            
     }
 
     const validURL = (str) => {
@@ -53,7 +133,7 @@ function Scraping() {
                 scrapWebsite(e)
             }} disabled={!validURL(updateUrl)}>Scrap website</button>
         </div>
-     {/* <div id={''}></div> */}
+     <div id={'scraper-container'}></div>
     </div>
   );
 }
